ed5431eab9e4b4b13caa458cadc147dc625782a9
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-06-11 11:16:24 +0530

    Day-2 {tryappone} creating app using Bootstrap.

---
+++ ALL_FILES/tryappone/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Tryapp1</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+   </>
+
+  );
+}
+
+export default App;
+
+
+//here we use Bootstrap for navbar
+//i change all the class tag into className by ctrl+f {find and replace for all}
+//i change all the href="#" tag into href="/" by ctrl+f {find and replace for all}
+//And all <> will change </>  or > to />

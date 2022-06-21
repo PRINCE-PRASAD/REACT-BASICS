9d2d858fa025f040e642ec9fe7d70d1a4b6f31d6
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-06-21 11:02:21 +0530

    State, New Component Navbar, eventlisener added

---
+++ tryappone_all_version/tryappone-v2/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+      {/* <Navbar title= "Tryapp" aboutText="About us"/> */}
+      <Navbar title="Tryapp" />
+    </>
+  );
+}
+
+export default App;

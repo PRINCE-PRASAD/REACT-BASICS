d7cc9cbef26bac82b53711d227daad36f6bf3219
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-06-22 20:28:51 +0530

    3 Component, Bootstrap, Unnecessary CSS Removed

---
+++ partrika_all_version/patrikav1/src/component/NewsItem.js
@@ -0,0 +1,13 @@
+//rce
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    return( <div>
+this s a news item
+    </div>
+    )
+  }
+}
+
+export default NewsItem;

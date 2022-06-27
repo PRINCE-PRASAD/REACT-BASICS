446c43c0fc4dbbfe86b5ff84bdd7fdda6865045c
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-06-27 21:47:57 +0530

    5news with loding gif

---
+++ partrika_all_version/patrikav3/src/component/NewsItem.js
@@ -0,0 +1,24 @@
+//rce
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+
+ 
+
+  render() {
+   let  {title, description, imageUrl, newsUrl} = this.props;
+    return( <div className="my-3">
+                    <div className="card" style={{width: "18rem"}}>
+                    <img src={!imageUrl?"https://pbs.twimg.com/profile_images/1108430392267280389/ufmFwzIn_400x400.png":imageUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                     <h5 className="card-title">{title}</h5>
+                     <p className="card-text">{description}</p>
+                      <a href={newsUrl} target={"_blank"} rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                   </div>
+                  </div>
+    </div>
+    )
+  }
+}
+
+export default NewsItem;

068bfdccf72b6e80066840c7da20cf711902a19d
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-07-26 16:29:19 +0530

    adding bcryptsjs for security of password in database

--- data_keep_all_version/data_keep/backend/routes/auth.js
+++ data_keep_all_version/data_keep/backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
 // create a user using: post "/api/auth/createuser" doesn't require auth
 
@@ -27,10 +28,14 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user alredy exist with this email" });
       }
+      // for adding salt(bcrypt)/hashing for saftey in password in database
+      const salt = await bcrypt.genSalt(10);
+      //for password adding with hash
+      const secPass = await bcrypt.hash(req.body.password, salt);
       //create a new user
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
 

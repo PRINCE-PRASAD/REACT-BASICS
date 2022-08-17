2b966a4fb866dfe07c7927047b30880db5856644
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-08-17 09:46:11 +0530

    ADDING NOTES IS NOW FUNCTIONAL

--- data_keep_all_version/data_keep/src/component/Notes.js
+++ data_keep_all_version/data_keep/src/component/Notes.js
@@ -2,13 +2,16 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 import Noteitem from "./Noteitem";
+import { AddNote } from "./AddNote";
 
 
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const {notes, setNotes} = context;
+    const {notes, addNote} = context;
   return (
+    <>
+    <AddNote />
     <div className="row my-3">
     <h2>Your Notes</h2>
    
@@ -17,6 +20,7 @@ const Notes = () => {
     }
     )}
     </div>
+    </>
   )
 
 }
--- data_keep_all_version/data_keep/src/context/notes/NoteState.js
+++ data_keep_all_version/data_keep/src/context/notes/NoteState.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 const NoteState = (props) => {
  const notesInitial= [
     {
-      "_id": "62e3ab0181d3b73bb97a2f6b",
+      "_id": "62e3ab0181d3b73bb97a2f6b0",
       "user": "62e0291bdb2db06630ba41c5",
       "title": "My Title",
       "description": "Here is my first desceiption",
@@ -52,8 +52,34 @@ const NoteState = (props) => {
   ]
 const [notes, setNotes] = useState(notesInitial);
 
+//Add a note
+const addNote = (title, description, tag)=>{
+  console.log("Adding a new note")
+const note={
+  "_id": "62e3ab0181d3b73bb97a2f6b5",
+      "user": "62e0291bdb2db06630ba41c5",
+      "title": "title",
+      "description": description,
+      "tag": tag,
+      "date": "2022-07-29T09:40:17.663Z",
+      "__v": 0
+};
+setNotes(notes.concat(note))
+}
+
+//Delete a note
+const deleteNote = ()=>{
+  
+}
+
+//Edit a note
+const EditNote = ()=>{
+  
+}
+
+
   return (
-    <NoteContext.Provider value={{notes, setNotes}}>
+    <NoteContext.Provider value={{notes, addNote, deleteNote, EditNote }}>
       {props.children}
     </NoteContext.Provider>
   );

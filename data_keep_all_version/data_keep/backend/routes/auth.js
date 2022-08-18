249fe390ad61b8e9887b1ae59d3978dd80625e7a
PRINCE-PRASAD <princeprasad.code@gmail.com>
2022-08-18 16:40:00 +0530

    Signup in database and used bootstrap for signup form

--- data_keep_all_version/data_keep/backend/routes/auth.js
+++ data_keep_all_version/data_keep/backend/routes/auth.js
@@ -18,10 +18,11 @@ router.post(
     body("password", "Enter a vaild password").isLength({ min: 5 }),
   ],
   async (req, res) => {
+    let success = false;
     //if there is an error , return the bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
 
     //check wheather the user wih this email exist or not
@@ -30,7 +31,7 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ error: "Sorry a user alredy exist with this email" });
+          .json({ success, error: "Sorry a user alredy exist with this email" });
       }
       // for adding salt(bcrypt)/hashing for saftey in password in database
       const salt = await bcrypt.genSalt(10);
@@ -51,7 +52,9 @@ router.post(
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
       //  -------------------------------------------------
-      res.json({ authtoken });
+      
+      success =true;
+      res.json({success, authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some Error Ocurred");
